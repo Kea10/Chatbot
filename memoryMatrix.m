af73e837ae1fcccff9db568d048a0a576920fd37
Kea10 <klwood2@asu.edu>
2017-03-10 11:32:07 -0700

    Added memoryMatrix methods
    I added an init method to the memory matrix and finished the index/word
    translator methods.

--- memoryMatrix.m
+++ memoryMatrix.m
@@ -9,18 +9,38 @@ classdef memoryMatrix
     
     methods
         function Init(obj)
-            
+            Ball = Word('ball', 1);
+            Cool = Word('cool', 2);
+            %Test words
+            obj.wordMem = [Ball, Cool];
+            obj.Mem = zeros(size(obj.wordMem), size(obj.wordMem));
         end
         function word = indexToWord(obj, n)
-            
+            for idx = 1:numel(wordMem)
+                element = wordMem(idx);
+                if element.Index == n
+                    word = element.Name
+                end
+            end
         end
         function index = wordToIndex(obj, n)
-            
+            for idx = 1:numel(wordMem)
+                element = wordMem(idx);
+                if element.Name == n
+                    index = element.Index;
+                end
+            end
         end
         function correlation = getCorrelation(obj, a, b)
             a = wordToIndex(a);
             b = wordToIndex(b);
-            correlation = Mem(a,b);
+            correlation = 1;
+            if b > a
+                correlation = obj.Mem(a,b);
+            end
+            if a > b
+                correlation = obj.Mem(b,a);
+            end
             %Might be changed based on future uses of the memMatrix 
             %i.e the bottom-left might be different than the upper-right
             %portion
