14ace5195a8901fae45a0fe5a57497e7d76ac8b6
Kea10 <klwood2@asu.edu>
2017-03-14 09:03:52 -0700

    Added descriptive comments
    Added comments to each of the classes.

--- memoryMatrix.m
+++ memoryMatrix.m
@@ -1,25 +1,46 @@
 classdef memoryMatrix
     %The memory of the chatbot put into the form of a matrix
-    %Used to make associations
+    %Used to make associations between words so that the chatbot can create
+    %sentences and come to conclusions.
     
     properties
+        
+        % Mem is a matrix used to represent the main memory of the chatbot. 
+        % The memory remembers correlations between words so that the
+        % chatbot can use them to create sentences and come to conclusions.
+        
         Mem
+        
+        % wordMem is an array of words used as an index for the mem matrix
+        % in order to correlate a position on the matrix with two words.
+        
         wordMem
     end
-    
+   
     methods
+        
+        % A list of get/set methods for Mem and wordMem
+        
         function obj = set.Mem(obj, value)
             obj.Mem = value;
         end
+        
         function obj = set.wordMem(obj, value)
             obj.wordMem = value; 
         end
+        
         function value = get.Mem(obj)
             value = obj.Mem;
         end
+        
         function value = get.wordMem(obj)
             value = obj.wordMem;
         end
+        
+        % An Init method for the matrix. Right now it is just sets up a
+        % bunch of predefined words, but it will eventually read/write to a
+        % text document. 
+        
         function obj = Init(obj)
             Ball = Word('ball', 1);
             Cool = Word('cool', 2);
@@ -29,6 +50,11 @@ classdef memoryMatrix
             sz = sz(1,2);
             obj.Mem = zeros([sz, sz]);
         end
+        
+        % The next two functions take an index or a string respectivly and
+        % turn them in to the other using the wordMem array. It is a
+        % convient function for other methods.
+        
         function word = indexToWord(obj, n)
             for idx = 1:numel(obj.wordMem)
                 element = obj.wordMem(idx);
@@ -37,6 +63,7 @@ classdef memoryMatrix
                 end
             end
         end
+        
         function index = wordToIndex(obj, n)
             for idx = 1:numel(obj.wordMem)
                 element = obj.wordMem(idx);
@@ -45,6 +72,10 @@ classdef memoryMatrix
                 end
             end
         end
+        
+        % A handy function to get a correlation between two strings in the
+        % memory matrix.
+        
         function correlation = getCorrelation(obj, a, b)
             a = wordToIndex(a);
             b = wordToIndex(b);
@@ -56,14 +87,20 @@ classdef memoryMatrix
                 correlation = obj.Mem(b,a);
             end
         end
+        
+        % Sets the correlation between two strings in the memory matrix to a certain value.
+        
         function obj = setCorrelation(obj, a, b, correlation)
             a = wordToIndex(a);
             b = wordToIndex(b);
+            
             %Will probally add a bool to determine whether it will be added
             %to bottom-left or top-right memory
+            
             if b > a
                 obj.Mem(a,b) = correlation;
             end
+            
             if a > b
                 obj.Mem(b,a) = correlation;
             end
