99a53e85b69d85e0984fc4d28887f78a2046c12c
Kea10 <klwood2@asu.edu>
2017-03-28 11:29:49 -0700

    Added memoryBank class
    Haven’t done anything with it yet, but it’s there.

---
+++ memoryBank.m
@@ -0,0 +1,14 @@
+classdef memoryBank
+    %UNTITLED3 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        bank
+    end
+    
+    methods
+        
+    end
+    
+end
+
