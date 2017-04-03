53624af75422d1fc1650b15871036ae245c9d1dc
Kea10 <klwood2@asu.edu>
2017-04-03 11:03:57 -0700

    Set Up memoryBank methods

--- Main.m
+++ Main.m
@@ -1,2 +1,10 @@
+% Init
 memory = memoryMatrix;
 memory = memory.Init();
+% Load Content
+
+% Loop
+
+% Update Loop
+
+% Draw Loop
--- memoryBank.m
+++ memoryBank.m
@@ -4,10 +4,29 @@ classdef memoryBank
     
     properties
         bank
+        capacity
     end
     
     methods
-        
+        function obj = set.bank(obj, value)
+            obj.bank = value;
+        end
+        function value = get.bank(obj)
+            value = obj.bank;
+        end
+        function obj = set.capacity(obj, value)
+            obj.capacity = value;
+        end
+        function value = get.capacity(obj)
+            value = obj.capacity;
+        end
+        function a = addValue(obj, data, weight)
+            for i = 1:numel(bank)
+                element = array(i);
+                
+            end
+        end
+            
     end
     
 end
